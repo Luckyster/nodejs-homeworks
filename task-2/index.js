ed7c552ea2cfd50a6cc6a06b01263f3a31f05cb9
Luckyster <gleb.phrolow@gmail.com>
2021-08-03 16:55:32 +0300

    refactor: put in events for constructor

--- task-2/index.js
+++ task-2/index.js
@@ -4,77 +4,109 @@ const {personModel} = require('./models/person');
 class Bank extends EventEmitter {
 	constructor() {
 		super();
-		this.persons = [];
+		this.persons = new Map();
+		this.ids = 0;
+		this.add();
+		this.withdraw();
+		this.get();
+		this.changeLimit();
+		this.error();
+	}
 
+	add() {
 		this.on('add', (id, admission) => {
-			if (admission <= 0) {
-				return this.emit('error', new Error('Admision less or equal to 0'));
-			}
-			if (!this.persons[id]) {
-				return this.emit('error', new Error('There is no person by that ID'));
-			}
-			let balance = this.persons[id]?.balance;
-			if (balance > 0) {
-        const updatedBalance = balance + admission;
-				this.persons[id].balance = updatedBalance;
-			} else {
-				return this.emit('error', new Error("No Balance"));
-			}
-		});
-
-		this.on('withdraw', (id, withdrawal) => {
-			if (withdrawal <= 0) {
-				return this.emit('error', new Error('Withdrawal less or equal to 0'));
-			}
-			if (!this.persons[id]) {
-				return this.emit('error', new Error('There is no person by that ID'));
-			}
-			let balance = this.persons[id]?.balance;
-
-			if (balance > 0) {
-				const updatedBalance = balance - withdrawal;
-				if(!this.persons[id]?.limit(withdrawal,balance ,updatedBalance)){
-          return this.emit('error', new Error('Exceeding the limit'));
-        }
-				if (balance - withdrawal < 0) {
-					return this.emit('error', new Error('Not Enough Money'));
+			try {
+				if (admission <= 0) {
+					throw new Error('Admision less or equal to 0');
 				}
-				this.persons[id].balance = updatedBalance;
-			} else {
-				return this.emit('error', new Error('No Balance'));
+				const person = this.persons.get(id);
+				const balance = person?.balance;
+				if (balance <= 0) {
+					throw new Error("No Person by that ID");
+				}
+				const updatedBalance = balance + admission;
+				this.persons.set(id, {...person, balance: updatedBalance});
+			} catch (e) {
+				return this.emit('error', e);
 			}
 		});
+	}
 
+	withdraw() {
+		this.on('withdraw', (id, withdrawal) => {
+			try {
+				if (withdrawal <= 0) {
+					throw new Error('Withdrawal less or equal to 0');
+				}
+				const person = this.persons.get(id);
+				if (!person) {
+					throw new Error('There is no person by that ID');
+				}
+				const balance = person?.balance;
+
+				if (balance <= 0) {
+					throw new Error('No Balance');
+				}
+				const updatedBalance = balance - withdrawal;
+				if (person?.hasOwnProperty('limit') && !person?.limit(withdrawal, balance, updatedBalance)) {
+					throw new Error('Exceeding the limit');
+				}
+				if (updatedBalance < 0) {
+					throw new Error('Not Enough Money');
+				}
+				this.persons.set(id, {...person, balance: updatedBalance});
+			} catch (e) {
+				return this.emit('error', e);
+			}
+
+		});
+	}
+
+	get() {
 		this.on('get', (id, callback) => {
-			let balance = this.persons[id]?.balance;
-			if (!this.persons[id]) {
-				return this.emit('error', new Error('There is no person by that ID'));
-			}
-			if (balance) {
+			try {
+				const person = this.persons.get(id);
+				const balance = person?.balance;
+				if (!person) {
+					throw new Error('There is no person by that ID');
+				}
+				if (!balance) {
+					throw new Error('No Balance');
+				}
 				return callback(balance)
-			} else {
-				return this.emit('error', new Error("No Balance"));
+			} catch (e) {
+				return this.emit('error', e);
 			}
 		});
+	}
 
+	send() {
 		this.on('send', (personFirstId, personSecondId, admission) => {
-			if (admission <= 0) {
-				return this.emit('error', new Error('Admission less or equal to 0'));
+			try {
+				if (admission <= 0) {
+					throw new Error('Admission less or equal to 0');
+				}
+				if (!this.persons.get(personFirstId) || !this.persons.get(personSecondId)) {
+					throw new Error('There is no person by that ID');
+				}
+				this.emit('withdraw', personFirstId, admission);
+				this.emit('add', personSecondId, admission);
+			} catch (e) {
+				return this.emit('error', e);
 			}
-			if (!this.persons[personFirstId] || !this.persons[personSecondId]) {
+		});
+	}
+
+	changeLimit() {
+		this.on('changeLimit', (id, callback) => {
+			if (!this.persons.get(id)) {
 				return this.emit('error', new Error('There is no person by that ID'));
 			}
-			this.emit('withdraw', personFirstId, admission);
-			this.emit('add', personSecondId, admission);
+			this.persons.get(id).limit = callback;
 		});
 
-    this.on('changeLimit', (id, callback) => {
-      if (!this.persons[id]) {
-        return this.emit('error', new Error('There is no person by that ID'));
-      }
-      this.persons[id].limit = callback;
-    });
-
+	}
+	error(){
 		this.on('error', error => {
 			console.error(
 					`Received ${error.name} with a message: '${error.message}'
@@ -82,37 +114,47 @@ class Bank extends EventEmitter {
 			);
 		});
 	}
+	_validation(valObject, model) {
+		try {
+			for (const [key, val] of Object.entries(model)) {
+				const property = valObject[key];
+				if (val.required === true && !property) {
+					throw new Error('No Required Filed');
+				}
+				if (
+						(val.type === 'array' && !Array.isArray(property)) ||
+						(val.type !== 'array' && valObject[key] && typeof property !== val.type)
+				) {
+					throw new Error('Wrong Type');
+				}
+				if (val['unique'] && this._findByProperty(valObject[key], key)) {
 
-	register(person) {
-		for (const [key, val] of Object.entries(personModel)) {
-			if (val.required === true && !person.hasOwnProperty(key)) {
-				return this.emit('error', new Error('No Required Filed'));
+					throw new Error('Name already exist');
+				}
+				if (val['min'] && property <= val.min) {
+					throw new Error(`${key} less then or equal to ${val.min}`);
+				}
+				if (val['max'] && property > val.max) {
+					throw new Error(`${key} more then ${val.max}`);
+				}
 			}
-			if (
-					(val.type === 'array' && !Array.isArray(person[key])) ||
-					(val.type !== 'array' && person[key] && typeof person[key] !== val.type)
-			) {
-				return this.emit('error', new Error('Wrong Type'));
-			}
-			if (personModel['balance'].hasOwnProperty('min') && person[key] <= val.min) {
-				return this.emit('error', new Error(`${key} less then or equal to ${val.min}`));
-			}
-			if (personModel['balance'].hasOwnProperty('max') && person[key] > val.max) {
-				return this.emit('error', new Error(`${key} more then ${val.max}`));
-			}
+		} catch (e) {
+			return this.emit('error', e);
 		}
-		if (this._findByProperty(person.name)) {
-			return this.emit('error', new Error('Name already exist'));
-		}
-		this.persons.push(person);
-    console.log('this.persons.indexOf(person)',this.persons.indexOf(person))
-		return this.persons.indexOf(person);
 	}
 
-	_findByProperty(name) {
-		for (const [index, val] of this.persons.entries()) {
-			if (val.name === name) {
-				return {index: index, timer: val};
+	register(person) {
+		this._validation(person, personModel);
+		const id = this.ids;
+		this.persons.set(id, person);
+		this.ids++;
+		return id;
+	}
+
+	_findByProperty(value, property) {
+		for (const [id, val] of this.persons.entries()) {
+			if (val[property] === value) {
+				return {id, person: val};
 			}
 		}
 		return false;
@@ -151,25 +193,25 @@ const bank = new Bank();
 // });
 
 const personID = bank.register({
-  name: 'Oliver White',
-  balance: 911,
-  limit: amount => amount < 10
+	name: 'Oliver White',
+	balance: 911,
+	limit: amount => amount < 10
 });
 const personID2 = bank.register({
 	name: 'Pitter Black',
 	balance: 100,
 });
 bank.emit('get', personID, (amount) => {
-  console.log(`I have ${amount}₴`); // I have 695₴
+	console.log(`I have ${amount}₴`); // I have 695₴
 });
 // Вариант 4
 bank.emit('changeLimit', personID, (amount, currentBalance,
-                                    updatedBalance) => {
-  return updatedBalance > 900;
+																		updatedBalance) => {
+	return updatedBalance > 900;
 });
-bank.emit('send', personID, personID2, 10); // Error
+bank.emit('send', personID, personID2, 9); // Error
 console.log('personID', personID);
 console.log('personID2', personID2);
 bank.emit('get', personID, (amount) => {
-  console.log(`I have ${amount}₴`); // I have 695₴
+	console.log(`I have ${amount}₴`); // I have 695₴
 });
--- task-2/models/person.js
+++ task-2/models/person.js
@@ -1,5 +1,5 @@
 exports.personModel = {
-  name: { required: true, type: 'string' },
+  name: {unique: true, required: true, type: 'string' },
   balance: { required: true, type: 'number', min: 0 },
   limit: {  type: 'function' },
 };
